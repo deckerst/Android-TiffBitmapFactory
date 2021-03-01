371821b908797c34dc9d12f48e4fbd22ed75a363
Thibault Deckers <thibault.deckers@gmail.com>
2021-03-01 22:02:34 +0900

    Squashed commit of the following:
    format + refactored filtering methods + apply filter only when subsampling + fixed copy/paste issues for TIFFTAG_TILELENGTH, FILLORDER_LSB2MSB

--- lib/src/main/cpp/include/NativeDecoder.h
+++ lib/src/main/cpp/include/NativeDecoder.h
@@ -16,115 +16,141 @@
 #include "NativeExceptions.h"
 
 #ifdef NDEBUG
-    #define LOGI(x)
-    #define LOGII(x, y)
-    #define LOGIF(x, y)
-    #define LOGIS(x, y)
-    #define LOGE(x)
-    #define LOGES(x, y)
-    #define LOGEI(x, y)
+#define LOGI(x)
+#define LOGII(x, y)
+#define LOGIF(x, y)
+#define LOGIS(x, y)
+#define LOGE(x)
+#define LOGES(x, y)
+#define LOGEI(x, y)
 #else
-    #define LOGI(x) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s", x)
-    #define LOGII(x, y) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s %d", x, y)
-    #define LOGIF(x, y) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s %f", x, y)
-    #define LOGIS(x, y) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s %s", x, y)
-    #define LOGE(x) __android_log_print(ANDROID_LOG_ERROR, "NativeDecoder", "%s", x)
-    #define LOGES(x, y) __android_log_print(ANDROID_LOG_ERROR, "NativeDecoder", "%s %s", x, y)
-    #define LOGEI(x, y) __android_log_print(ANDROID_LOG_ERROR, "NativeDecoder", "%s %d", x, y)
+#define LOGI(x) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s", x)
+#define LOGII(x, y) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s %d", x, y)
+#define LOGIF(x, y) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s %f", x, y)
+#define LOGIS(x, y) __android_log_print(ANDROID_LOG_DEBUG, "NativeDecoder", "%s %s", x, y)
+#define LOGE(x) __android_log_print(ANDROID_LOG_ERROR, "NativeDecoder", "%s", x)
+#define LOGES(x, y) __android_log_print(ANDROID_LOG_ERROR, "NativeDecoder", "%s %s", x, y)
+#define LOGEI(x, y) __android_log_print(ANDROID_LOG_ERROR, "NativeDecoder", "%s %d", x, y)
 #endif
 
-class NativeDecoder
-{
-    public:
-        explicit NativeDecoder(JNIEnv *, jclass, jint, jobject, jobject);
-        ~NativeDecoder();
-        jobject getBitmap();
+class NativeDecoder {
+public:
+    explicit NativeDecoder(JNIEnv *, jclass, jint, jobject, jobject);
 
-    private:
-        //constants
-        static int const colorMask = 0xFF;
-        static int const ARGB_8888 = 2;
-        static int const RGB_565 = 4;
-        static int const ALPHA_8 = 8;
+    ~NativeDecoder();
 
-        static int const DECODE_METHOD_IMAGE = 1;
-        static int const DECODE_METHOD_TILE = 2;
-        static int const DECODE_METHOD_STRIP = 3;
+    jobject getBitmap();
 
-        static int const DECODE_MODE_FILE_PATH = 1;
-        static int const DECODE_MODE_FILE_DESCRIPTOR = 2;
+private:
+    //constants
+    static uint const colorMask = 0xFF;
+    static int const ARGB_8888 = 2;
+    static int const RGB_565 = 4;
+    static int const ALPHA_8 = 8;
 
-        //decoding mode
-        int decodingMode;
+    static int const DECODE_METHOD_IMAGE = 1;
+    static int const DECODE_METHOD_TILE = 2;
+    static int const DECODE_METHOD_STRIP = 3;
 
-        //fields
-        JNIEnv *env;
-        jclass clazz;
+    static int const DECODE_MODE_FILE_PATH = 1;
+    static int const DECODE_MODE_FILE_DESCRIPTOR = 2;
 
-        static jmp_buf tile_buf;
-        static jmp_buf strip_buf;
-        static jmp_buf image_buf;
-        static jmp_buf general_buf;
+    //decoding mode
+    int decodingMode;
 
-        jobject optionsObject;
-        jobject listenerObject;
-        jclass jIProgressListenerClass;
-        jclass jBitmapOptionsClass;
-        jclass jThreadClass = NULL;
-        jint jFd;
-        jstring jPath;
-        jboolean throwException;
-        jboolean useOrientationTag;
-        TIFF *image;
-        jlong progressTotal;
-        int origwidth;
-        int origheight;
-        short origorientation;
-        int origcompressionscheme;
-        jobject preferedConfig;
-        jboolean invertRedAndBlue;
-        jint boundX;
-        jint boundY;
-        jint boundWidth;
-        jint boundHeight;
-        char hasBounds;
-        unsigned long availableMemory;
-        //methods
-        int getDyrectoryCount();
-        void writeDataToOptions(int);
-        jobject createBitmap(int, int);
-        jint *getSampledRasterFromImage(int, int *, int *);
-        jint *getSampledRasterFromImageWithBounds(int , int *, int *);
-        jint *getSampledRasterFromStrip(int, int *, int *);
-        jint *getSampledRasterFromStripWithBounds(int, int *, int *);
-        void rotateTileLinesVertical(uint32, uint32, uint32 *, uint32 *);
-        void rotateTileLinesHorizontal(uint32, uint32, uint32 *, uint32 *);
-        void flipPixelsVertical(uint32, uint32, jint *);
-        void flipPixelsHorizontal(uint32, uint32, jint *);
-        jint *getSampledRasterFromTile(int, int *, int *);
-        jint *getSampledRasterFromTileWithBounds(int, int *, int *);
-        int getDecodeMethod();
-        void fixOrientation(jint *, uint32, int, int);
-        void rotateRaster(jint *, int, int *, int *);
-        jbyte * createBitmapAlpha8(jint *, int, int);
-        unsigned short *createBitmapRGB565(jint *, int, int);
-        jstring charsToJString(const char *);
-        jboolean checkStop();
-        void sendProgress(jlong, jlong);
-        //functions for catching SIGSEGV from libtiff methods
-        void signalFromTileHandler(int code, siginfo_t *siginfo, void *sc);
-        void signalFromStripHandler(int code, siginfo_t *siginfo, void *sc);
+    //fields
+    JNIEnv *env;
 
-        //throwing exceptions
-        void throwDecodeFileException(const char *);
-        void throwCantOpenFileException();
+    static jmp_buf tile_buf;
+    static jmp_buf strip_buf;
+    static jmp_buf image_buf;
+    static jmp_buf general_buf;
 
-        static void tileErrorHandler(int code, siginfo_t *siginfo, void *sc);
-        static void stripErrorHandler(int code, siginfo_t *siginfo, void *sc);
-        static void imageErrorHandler(int code, siginfo_t *siginfo, void *sc);
-        static void generalErrorHandler(int code, siginfo_t *siginfo, void *sc);
+    jobject optionsObject;
+    jobject listenerObject;
+    jclass jIProgressListenerClass;
+    jclass jBitmapOptionsClass;
+    jclass jThreadClass = NULL;
+    jint jFd;
+    jstring jPath;
+    jboolean throwException;
+    jboolean useOrientationTag;
+    TIFF *image;
+    jlong progressTotal;
+    int origwidth;
+    int origheight;
+    short origorientation;
+    int origcompressionscheme;
+    jobject preferedConfig;
+    jboolean invertRedAndBlue;
+    jint boundX;
+    jint boundY;
+    jint boundWidth;
+    jint boundHeight;
+    char hasBounds;
+    unsigned long availableMemory;
+
+    //methods
+    int getDirectoryCount();
+
+    void writeDataToOptions(int);
+
+    jobject createBitmap(int, int);
+
+    jint *getSampledRasterFromImage(int, int *, int *);
+
+    jint *getSampledRasterFromImageWithBounds(int, int *, int *);
+
+    jint applyFilterForImage(int x, int y, const unsigned int *raster) const;
+
+    jint *getSampledRasterFromStrip(int, int *, int *);
+
+    jint *getSampledRasterFromStripWithBounds(int, int *, int *);
+
+    jint applyFilterForStrip(int x, int y, const uint32 *raster, const unsigned int *matrixTopLine, const unsigned int *matrixBottomLine, int rowPerStrip, int globalLineCounter, int isSecondRasterExist) const;
+
+    jint *getSampledRasterFromTile(int, int *, int *);
+
+    jint *getSampledRasterFromTileWithBounds(int, int *, int *);
+
+    jint applyFilterForTile(int x, int y, const uint32 *rasterTile, const uint32 *rasterTileLeft, const uint32 *rasterTileRight, uint32 tileWidth, uint32 tileHeight, short leftTileExists, short rightTileExists) const;
+
+    int getDecodeMethod();
+
+    void rotateTileLinesVertical(uint32, uint32, uint32 *, uint32 *);
+
+    void rotateTileLinesHorizontal(uint32, uint32, uint32 *, uint32 *);
+
+    void flipPixelsVertical(uint32, uint32, jint *);
+
+    void flipPixelsHorizontal(uint32, uint32, jint *);
+
+    void fixOrientation(jint *, uint32, int, int);
+
+    void rotateRaster(jint *, int, int *, int *);
+
+    jbyte *createBitmapAlpha8(jint *, int, int);
+
+    unsigned short *createBitmapRGB565(jint *, int, int);
+
+    jstring charsToJString(const char *);
+
+    jboolean checkStop();
+
+    void sendProgress(jlong, jlong);
+
+    //throwing exceptions
+    void throwDecodeFileException(const char *);
+
+    void throwCantOpenFileException();
+
+    static void tileErrorHandler(int code, siginfo_t *siginfo, void *sc);
+
+    static void stripErrorHandler(int code, siginfo_t *siginfo, void *sc);
+
+    static void imageErrorHandler(int code, siginfo_t *siginfo, void *sc);
+
+    static void generalErrorHandler(int code, siginfo_t *siginfo, void *sc);
 };
-
-
 
 #endif //TIFFSAMPLE_NATIVEDECODER_H
